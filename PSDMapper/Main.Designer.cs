97e902f88015cf5be7c02ce09663f0eac37297a0
BrunoLM <brunolm7@gmail.com>
2011-08-27 01:00:24 -0300

    Startup

---
+++ PSDMapper/Main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PSDMapper
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.output = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.pin = new System.Windows.Forms.ToolStripButton();
+            this.copy = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(0, 28);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(702, 448);
+            this.output.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copy,
+            this.toolStripSeparator1,
+            this.pin});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(702, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // pin
+            // 
+            this.pin.Checked = true;
+            this.pin.CheckOnClick = true;
+            this.pin.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.pin.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.pin.Image = ((System.Drawing.Image)(resources.GetObject("pin.Image")));
+            this.pin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.pin.Name = "pin";
+            this.pin.Size = new System.Drawing.Size(73, 22);
+            this.pin.Text = "&Pin window";
+            this.pin.Click += new System.EventHandler(this.pin_Click);
+            // 
+            // copy
+            // 
+            this.copy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.copy.Image = ((System.Drawing.Image)(resources.GetObject("copy.Image")));
+            this.copy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(39, 22);
+            this.copy.Text = "&Copy";
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 476);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.output);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "PSD Mapper";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton pin;
+        private System.Windows.Forms.ToolStripButton copy;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ PSDMapper/Main.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using PhotoshopFile;
+using PSDMapper.Properties;
+
+namespace PSDMapper
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            TopMost = true;
+            AllowDrop = true;
+        }
+
+        private void Main_Load(object sender, EventArgs ev)
+        {
+            #region Form location
+
+            Location = Settings.Default.StartLocation;
+
+            FormClosing += (s, e) =>
+            {
+                Settings.Default.StartLocation = Location;
+                Settings.Default.Save();
+            };
+
+            #endregion
+
+            DragEventHandler enter = (s, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string[] files = e.Data.GetData(DataFormats.FileDrop, false) as string[];
+                    if (files.FirstOrDefault<string>().EndsWith(".psd", StringComparison.OrdinalIgnoreCase))
+                    {
+                        e.Effect = DragDropEffects.Copy;
+                    }
+                }
+            };
+            DragEventHandler drop = (s, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    string file = (e.Data.GetData(DataFormats.FileDrop, false) as string[]).FirstOrDefault<string>();
+                    PsdFile ps = new PsdFile();
+                    ps.Load(file);
+                    MapLayers(ps);
+                }
+            };
+
+            base.DragEnter += enter;
+            base.DragDrop += drop;
+        }
+
+        public void MapLayers(PsdFile ps)
+        {
+            output.Clear();
+
+            ps.Layers.OrderBy(l => l.Rect.X).ThenBy(l => l.Rect.Y)
+                .ToList()
+                .ForEach(l =>
+                {
+                    output.AppendText(
+                        String.Format(".{0} {{ width: {1}px; height: {2}px; background-position: {3}px {4}px; }}{5}",
+                            l.Name,
+                            l.Rect.Width,
+                            l.Rect.Height,
+                            l.Rect.X != 0 ? -l.Rect.X : 0,
+                            l.Rect.Y != 0 ? -l.Rect.Y : 0,
+                            Environment.NewLine
+                        )
+                    );
+                });
+        }
+
+        private void pin_Click(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+        }
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(output.Text);
+        }
+    }
+}
